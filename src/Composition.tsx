f8a5a0f0d325090dbc25728e7f3082ca129b8cde
Jonny Burger <jonathanburger11@gmail.com>
2024-05-08 09:27:25 +0200

    start light leaks

--- src/Composition.tsx
+++ src/Composition.tsx
@@ -1,3 +1,34 @@
+import { TransitionSeries, linearTiming } from "@remotion/transitions";
+import { Img, staticFile } from "remotion";
+import { fade } from "@remotion/transitions/fade";
+
 export const MyComposition = () => {
-  return null;
+  return (
+    <TransitionSeries>
+      <TransitionSeries.Sequence durationInFrames={100}>
+        <Img
+          style={{
+            objectFit: "cover",
+            width: "100%",
+          }}
+          src={staticFile("a.jpg")}
+        />
+      </TransitionSeries.Sequence>
+      <TransitionSeries.Transition
+        timing={linearTiming({
+          durationInFrames: 40,
+        })}
+        presentation={fade()}
+      />
+      <TransitionSeries.Sequence durationInFrames={100}>
+        <Img
+          style={{
+            objectFit: "cover",
+            width: "100%",
+          }}
+          src={staticFile("b.jpg")}
+        />
+      </TransitionSeries.Sequence>
+    </TransitionSeries>
+  );
 };
